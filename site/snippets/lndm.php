64f9817bb87615fa3c584d6e659d0b49910ff275
CodingJo <jonathan.dudek@web.de>
2016-03-09 03:17:21 +0100

    lndm.php

--- site/snippets/lndm.php
+++ site/snippets/lndm.php
@@ -1,6 +1,6 @@
 <section id="<?php echo $data->uid() ?>">
 <div id="logo">
-<img src="../onepager-master/content/01-lndm/logo.png" class="responsive-image">
+<img src="../onepager/content/01-lndm/logo.png" class="responsive-image">
 </div>
 <div id="lndm2"
   <h1><?php echo $data->title()->html() ?></h1>
